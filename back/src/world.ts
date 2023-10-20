042b07cedb209fe1f9ababaa3946dda4de1ffe33
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-10-20 17:36:02 +0200

    back/lib: sequelize sqlite3

--- back/src/world.ts
+++ back/src/world.ts
@@ -1,1 +1,9 @@
-export class World {}
+import { Config, JsonDB } from "node-json-db";
+
+export class World {
+  db: JsonDB;
+
+  constructor() {
+    this.db = new JsonDB(new Config("world.json", true, false, "/"));
+  }
+}
