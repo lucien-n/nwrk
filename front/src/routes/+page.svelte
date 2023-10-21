19be8532fee0229360206a0ba4a19911caeade4c
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-10-21 13:12:12 +0200

    wip: connect and control a certain turtle

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { Loader2 } from 'lucide-svelte';
+
+	const getIds = async () => {
+		const res = await fetch(`http://localhost:8080/ids`);
+		const ids = res.json();
+		return ids;
+	};
+</script>
+
+<section class="w-full h-full flex flex-col gap-2 items-center justify-center">
+	{#await getIds()}
+		<h1 class="text-3xl">Fetching ids</h1>
+		<span class="animate-spin"><Loader2 /></span>
+	{:then ids}
+		{#each ids as id}
+			<Button href="/{id}"><p>Connect to <strong>{id}</strong></p></Button>
+		{/each}
+	{/await}
+</section>
