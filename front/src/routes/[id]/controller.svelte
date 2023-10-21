46cb17b704e9302865f75e1ba50c81374e4c7b73
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-10-21 13:46:13 +0200

    front/fix: handle refuel command

--- front/src/routes/[id]/controller.svelte
+++ front/src/routes/[id]/controller.svelte
@@ -21,7 +21,7 @@
 		<Compass />
 	</div>
 	<div class="absolute right-4 top-4">
-		<FuelGauge on:refuel={() => handleCommand('refuel')} />
+		<FuelGauge on:refuel={() => dispatch('refuel')} />
 	</div>
 
 	<div class="absolute left-4 bottom-4">
