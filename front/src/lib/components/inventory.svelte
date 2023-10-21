dfa7fc72b17a42f3d3cdb27fca4092af5feb34c7
lucien-neuhoff <lucien.neuhoff@protonmail.com>
2023-10-22 00:06:27 +0200

    front/fix: select slot on click

--- front/src/lib/components/inventory.svelte
+++ front/src/lib/components/inventory.svelte
@@ -3,23 +3,26 @@
 	import { createEventDispatcher } from 'svelte';
 	import Slot from './slot.svelte';
 
-	export let selected: number = 0;
-
 	const dispatch = createEventDispatcher();
 	const cooldown = createCoolDownStore(500);
 	const numberOfSlots = 16;
 
 	let timeout: number = 0;
+	let selected: number = 0;
 
 	const handleWheel = (ev: WheelEvent) => {
 		if (!controlsEnabled) return;
 		if (ev.deltaY < 0) selected = selected === 0 ? numberOfSlots - 1 : selected - 1;
 		if (ev.deltaY > 0) selected = selected === numberOfSlots - 1 ? 0 : selected + 1;
 
+		handleSelect();
+	};
+
+	const handleSelect = () => {
 		if (cooldown.isActive()) {
 			if (timeout) clearTimeout(timeout);
-			timeout = setTimeout(() => sync(), cooldown.timeLeft());
 		} else cooldown.trigger();
+		timeout = setTimeout(() => sync(), cooldown.timeLeft());
 	};
 
 	const sync = () => {
@@ -31,6 +34,13 @@
 
 <div class="grid grid-cols-4 grid-rows-4 gap-2">
 	{#each { length: numberOfSlots } as _, i}
-		<Slot slot={null} selected={i === selected} on:select={() => (selected = i)} />
+		<Slot
+			slot={null}
+			selected={i === selected}
+			on:select={() => {
+				selected = i;
+				handleSelect();
+			}}
+		/>
 	{/each}
 </div>
